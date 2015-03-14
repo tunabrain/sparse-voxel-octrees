5c7ac784287cb5bb611ef4a813e7297b0c2c50c0
Benedikt Bitterli <mail@noobody.org>
2015-03-14 10:24:13 +0100

    Changed line endings of tribox3.c to be compatible with MSVC

--- src/third-party/tribox3.c
+++ src/third-party/tribox3.c
@@ -1,38 +1,62 @@
-/********************************************************/
-/* AABB-triangle overlap test code                      */
-/* by Tomas Akenine-Möller                              */
-/* Function: int triBoxOverlap(float boxcenter[3],      */
-/*          float boxhalfsize[3],float triverts[3][3]); */
-/* History:                                             */
-/*   2001-03-05: released the code in its first version */
-/*   2001-06-18: changed the order of the tests, faster */
-/*                                                      */
-/* Acknowledgement: Many thanks to Pierre Terdiman for  */
-/* suggestions and discussions on how to optimize code. */
-/* Thanks to David Hunt for finding a ">="-bug!         */
-/********************************************************/
-#include <math.h>
-#include <stdio.h>
-
-#define X 0
-#define Y 1
-#define Z 2
-
+/********************************************************/
+
+/* AABB-triangle overlap test code                      */
+
+/* by Tomas Akenine-Möller                              */
+
+/* Function: int triBoxOverlap(float boxcenter[3],      */
+
+/*          float boxhalfsize[3],float triverts[3][3]); */
+
+/* History:                                             */
+
+/*   2001-03-05: released the code in its first version */
+
+/*   2001-06-18: changed the order of the tests, faster */
+
+/*                                                      */
+
+/* Acknowledgement: Many thanks to Pierre Terdiman for  */
+
+/* suggestions and discussions on how to optimize code. */
+
+/* Thanks to David Hunt for finding a ">="-bug!         */
+
+/********************************************************/
+
+#include <math.h>
+
+#include <stdio.h>
+
+
+
+#define X 0
+
+#define Y 1
+
+#define Z 2
+
+
+
 #define CROSS(dest,v1,v2) \
           dest[0]=v1[1]*v2[2]-v1[2]*v2[1]; \
           dest[1]=v1[2]*v2[0]-v1[0]*v2[2]; \
-          dest[2]=v1[0]*v2[1]-v1[1]*v2[0]; 
-#define DOT(v1,v2) (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2])
+          dest[2]=v1[0]*v2[1]-v1[1]*v2[0]; 
+
+#define DOT(v1,v2) (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2])
+
 #define SUB(dest,v1,v2) \
           dest[0]=v1[0]-v2[0]; \
           dest[1]=v1[1]-v2[1]; \
-          dest[2]=v1[2]-v2[2]; 
+          dest[2]=v1[2]-v2[2]; 
+
 #define FINDMINMAX(x0,x1,x2,min,max) \
   min = max = x0;   \
   if(x1<min) min=x1;\
   if(x1>max) max=x1;\
   if(x2<min) min=x2;\
-  if(x2>max) max=x2;
+  if(x2>max) max=x2;
+
 int planeBoxOverlap(float normal[3], float vert[3], float maxbox[3])	// -NJMP-
 {
   int q;
@@ -57,44 +81,54 @@ int planeBoxOverlap(float normal[3], float vert[3], float maxbox[3])	// -NJMP-
   return 0;
 }
 
-/*======================== X-tests ========================*/
+/*======================== X-tests ========================*/
+
 #define AXISTEST_X01(a, b, fa, fb)			   \
 	p0 = a*v0[Y] - b*v0[Z];			       	   \
 	p2 = a*v2[Y] - b*v2[Z];			       	   \
         if(p0<p2) {min=p0; max=p2;} else {min=p2; max=p0;} \
 	rad = fa * boxhalfsize[Y] + fb * boxhalfsize[Z];   \
-	if(min>rad || max<-rad) return 0;
+	if(min>rad || max<-rad) return 0;
+
 #define AXISTEST_X2(a, b, fa, fb)			   \
 	p0 = a*v0[Y] - b*v0[Z];			           \
 	p1 = a*v1[Y] - b*v1[Z];			       	   \
         if(p0<p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
 	rad = fa * boxhalfsize[Y] + fb * boxhalfsize[Z];   \
-	if(min>rad || max<-rad) return 0;
-/*======================== Y-tests ========================*/
+	if(min>rad || max<-rad) return 0;
+
+/*======================== Y-tests ========================*/
+
 #define AXISTEST_Y02(a, b, fa, fb)			   \
 	p0 = -a*v0[X] + b*v0[Z];		      	   \
 	p2 = -a*v2[X] + b*v2[Z];	       	       	   \
         if(p0<p2) {min=p0; max=p2;} else {min=p2; max=p0;} \
 	rad = fa * boxhalfsize[X] + fb * boxhalfsize[Z];   \
-	if(min>rad || max<-rad) return 0;
+	if(min>rad || max<-rad) return 0;
+
 #define AXISTEST_Y1(a, b, fa, fb)			   \
 	p0 = -a*v0[X] + b*v0[Z];		      	   \
 	p1 = -a*v1[X] + b*v1[Z];	     	       	   \
         if(p0<p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
 	rad = fa * boxhalfsize[X] + fb * boxhalfsize[Z];   \
-	if(min>rad || max<-rad) return 0;
-/*======================== Z-tests ========================*/#define AXISTEST_Z12(a, b, fa, fb)			   \
+	if(min>rad || max<-rad) return 0;
+
+/*======================== Z-tests ========================*/
+
+#define AXISTEST_Z12(a, b, fa, fb)			   \
 	p1 = a*v1[X] - b*v1[Y];			           \
 	p2 = a*v2[X] - b*v2[Y];			       	   \
         if(p2<p1) {min=p2; max=p1;} else {min=p1; max=p2;} \
 	rad = fa * boxhalfsize[X] + fb * boxhalfsize[Y];   \
-	if(min>rad || max<-rad) return 0;
+	if(min>rad || max<-rad) return 0;
+
 #define AXISTEST_Z0(a, b, fa, fb)			   \
 	p0 = a*v0[X] - b*v0[Y];				   \
 	p1 = a*v1[X] - b*v1[Y];			           \
         if(p0<p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
 	rad = fa * boxhalfsize[X] + fb * boxhalfsize[Y];   \
-	if(min>rad || max<-rad) return 0;
+	if(min>rad || max<-rad) return 0;
+
 int triBoxOverlap(float boxcenter[3],float boxhalfsize[3],float triverts[3][3])
 {
   /*    use separating axis theorem to test overlap between triangle and box */
