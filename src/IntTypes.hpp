e3b66d9bbe77a69b270d280bec7dce31f04978e2
Benedikt Bitterli <mail@noobody.org>
2015-03-23 17:46:58 +0100

    Added less verbose sized integer typedefs

---
+++ src/IntTypes.hpp
@@ -0,0 +1,39 @@
+/*
+Copyright (c) 2013 Benedikt Bitterli
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
+
+#ifndef INTTYPES_HPP_
+#define INTTYPES_HPP_
+
+#include <cstdint>
+
+typedef std::uint8_t  uint8;
+typedef std::uint16_t uint16;
+typedef std::uint32_t uint32;
+typedef std::uint64_t uint64;
+
+typedef std::int8_t  int8;
+typedef std::int16_t int16;
+typedef std::int32_t int32;
+typedef std::int64_t int64;
+
+#endif /* INTTYPES_HPP_ */
